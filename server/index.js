7997f38f3d8e4629278aeeba0f4b7a6f095ed571
jurek.ohrndorf <jurek.ohrndorf@check24.de>
2022-10-24 22:37:15 +0200

    testDB

--- server/index.js
+++ server/index.js
@@ -59,6 +59,16 @@ app.post("/api/image", upload.single("file"), function (req, res) {
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+  ComponentModel.create(
+    { title: "test123TITLE", path: "/.../image" },
+    function (err, instance) {
+      if (err) {
+        console.log("ERROR");
+        return handleError(err);
+      }
+      // saved!
+    }
+  );
 });
 
 app.post("/api/greet", function (req, res) {
