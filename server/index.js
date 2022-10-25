81a9c4958c2f72b661fd42de4c12f696012f7ab0
jurek.ohrndorf <jurek.ohrndorf@check24.de>
2022-10-25 13:56:52 +0200

    change log

--- server/index.js
+++ server/index.js
@@ -47,6 +47,7 @@ const upload = multer({ storage: storage });
 app.post("/api/image", upload.single("file"), function (req, res) {
   console.log("API IMAGE STARTET");
   console.log("File", req.file);
+  console.log("File", req.files);
   ComponentModel.create(
     { title: req.body.title, path: "/.../image" },
     function (err, instance) {
